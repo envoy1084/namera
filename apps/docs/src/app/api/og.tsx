60fc062b82834cd05cda3addac500fd3a22752be
Vedant Chainani <vedantchainani1084@gmail.com>
2026-03-25 01:15:12 +0530

    chore: use remote font url

--- apps/docs/src/app/api/og.tsx
+++ apps/docs/src/app/api/og.tsx
@@ -1,6 +1,3 @@
-import fs from "node:fs";
-import path from "node:path";
-
 import { createFileRoute } from "@tanstack/react-router";
 
 import { ImageResponse } from "@takumi-rs/image-response";
@@ -14,18 +11,20 @@ const OgSearchSchema = Schema.Struct({
 export const Route = createFileRoute("/api/og")({
   server: {
     handlers: {
-      GET: ({ request }) => {
+      GET: async ({ request }) => {
         const url = new URL(request.url);
         const searchParams = Object.fromEntries(url.searchParams.entries());
         const search = Schema.decodeUnknownSync(OgSearchSchema)(searchParams);
         const { title, description } = search;
-        const fontPath = path.join(
-          process.cwd(),
-          "public",
-          "fonts",
-          "HelveticaNowText-Regular.woff2",
+
+        const fontUrl = new URL(
+          "/fonts/HelveticaNowText-Regular.woff2",
+          request.url,
         );
-        const font = fs.readFileSync(fontPath);
+
+        const helveticaNowText = await fetch(fontUrl).then((res) =>
+          res.arrayBuffer(),
+        );
 
         return new ImageResponse(
           <div tw="flex flex-col gap-2">
@@ -35,7 +34,7 @@ export const Route = createFileRoute("/api/og")({
           {
             fonts: [
               {
-                data: font,
+                data: helveticaNowText,
                 name: "Helvetica Now Text",
                 style: "normal",
                 weight: 400,
